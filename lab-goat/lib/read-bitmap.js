337e2b374e31dbeec21fc851ef07d1dbe7e7e224
lonewalker72 <walkisai.72@gmail.com>
2017-08-24 22:06:09 -0700

    corrected syntax error in read-bitmap.js

--- lab-goat/lib/read-bitmap.js
+++ lab-goat/lib/read-bitmap.js
@@ -3,7 +3,10 @@
 const Bitmap = require ('../model/bitmap');
 const fs = require('fs');
 
-fs.readFile(`${__dirname}/../../assets/palette-bitmap.bmp`), (err, data) => {
-  if (err) console.err(err);
+// console.log(Bitmap());
+fs.readFile(`${__dirname}/../../assets/palette-bitmap.bmp`, (err, data) => {
+  if (err) console.error(err);
+  console.log('data', data);
   let bmp = new Bitmap(data);
-};
+  console.log(bmp);
+});
