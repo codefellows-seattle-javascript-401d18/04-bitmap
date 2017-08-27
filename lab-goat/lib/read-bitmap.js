8bbb79495ab865b516abbcce05cfbf24343f34d2
ZacharyJohnson <zaj6827@gmail.com>
2017-08-27 12:52:13 -0700

    commiting scaffolding and initial code

--- lab-goat/lib/read-bitmap.js
+++ lab-goat/lib/read-bitmap.js
@@ -3,7 +3,10 @@
 const Bitmap = require ('../model/bitmap');
 const fs = require('fs');
 
-fs.readFile(`${__dirname}/../../assets/palette-bitmap.bmp`), (err, data) => {
-  if (err) console.err(err);
-  let bmp = new Bitmap(data);
+module.exports = () => {
+  fs.readFile(`${__dirname}/../../assets/palette-bitmap.bmp`, (err, data) => {
+    if (err) console.err(err);
+    let bmp = new Bitmap(data);
+    console.log(bmp.pixelArray);
+  });
 };
